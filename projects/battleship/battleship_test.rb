30f51d186ef8b506c12a776fe6bb6f1c29ea3fee
Mason F. Matthews <mason.f.matthews@gmail.com>
2015-01-01 15:43:05 -0500

    Complete work on battleship test suite.

--- projects/battleship/battleship_test.rb
+++ projects/battleship/battleship_test.rb
@@ -4,10 +4,8 @@ require 'minitest/pride'
 require './battleship'
 
 $mock_inputs = []
-class Game
-  def get_user_input
-    $mock_inputs.shift
-  end
+def get_user_input
+  $mock_inputs.shift
 end
 
 class BattleshipTest < Minitest::Test
@@ -247,6 +245,19 @@ J |   |   |   |   |   |   |   |   |   |   |
 }
   end
 
+  def test_entire_board_can_be_sunk
+    board = Board.new
+    refute board.sunk?
+    board.place_ship(Ship.new(2), "D6", true)
+    refute board.sunk?
+    board.fire_at("D6")
+    refute board.sunk?
+    board.fire_at("D7")
+    assert board.sunk?
+  end
+
+
+
   def test_player_classes_exist
     assert Player
     assert HumanPlayer
@@ -273,6 +284,41 @@ J |   |   |   |   |   |   |   |   |   |   |
     assert_equal "HAL 9000", ComputerPlayer.new.name
   end
 
+  def test_players_have_boards
+    assert_equal Board, HumanPlayer.new.board.class
+    assert_equal Board, ComputerPlayer.new.board.class
+  end
+
+  def test_computer_player_automatically_places_ships
+    player = ComputerPlayer.new
+    assert_output("HAL 9000 has placed his ships.\n") do
+      assert player.place_ships([2, 3, 3, 4, 5])
+    end
+    assert_equal 5, player.ships.length
+    assert_equal 4, player.ships[3].length
+  end
+
+  def test_human_player_is_asked_to_place_ships
+    player = HumanPlayer.new
+    $mock_inputs.clear
+    $mock_inputs << "A1"
+    $mock_inputs << "Down"
+    $mock_inputs << "A4"
+    $mock_inputs << "Down"
+    assert_output("Dave, where would you like to place a ship of length 2?\nAcross or Down?\n"+
+                  "Dave, where would you like to place a ship of length 5?\nAcross or Down?\n") do
+      assert player.place_ships([2, 5])
+    end
+    assert_equal 2, player.ships.length
+    assert_equal 5, player.ships[1].length
+    assert player.board.has_ship_on?("A1")
+    assert player.board.has_ship_on?("A4")
+    assert player.board.has_ship_on?("B1")
+    refute player.board.has_ship_on?("C1")
+  end
+
+
+
   def test_game_class_exists
     assert Game
   end
@@ -287,37 +333,231 @@ J |   |   |   |   |   |   |   |   |   |   |
   end
 
   def test_game_welcomes_player
-    setup_new_game
+    human = HumanPlayer.new("Frank")
+    computer = ComputerPlayer.new
+    game = Game.new(human, computer)
     assert_output("Welcome, Frank and HAL 9000!\nIt's time to play Battleship.\n") do
-      @game.welcome
+      game.welcome
     end
   end
 
-  # def test_game_asks_human_to_place_ships
-  #   setup_new_game
-  #   assert_output("Frank, please place your ships.\n") do
-  #     @game.place_ships(@human)
-  #   end
-  # end
+  def test_game_can_ask_to_set_up_ships
+    set_up_new_game
 
-  # def
-  #   $mock_inputs << "exit"
-  #   assert game.setup
-  # end
+    assert_equal 5, @human.ships.length
+    assert @human.board.has_ship_on?("B1")
+    assert @human.board.has_ship_on?("C3")
+    assert @human.board.has_ship_on?("E9")
+    refute @human.board.has_ship_on?("E7")
 
-  # def test_misses_on_empty_board
-  #   board = Board.new
-  #   assert_output("Firing on A1...\nMiss!\n") do
-  #     refute board.fire_at("A1")
-  #   end
-  #   assert_output("Firing on G10...\nMiss!\n") do
-  #     refute board.fire_at("G10")
-  #   end
-  # end
+    assert_equal 5, @computer.ships.length
+    assert_equal 4, @computer.ships[3].length
+  end
 
-  def setup_new_game
+  def set_up_new_game
     @human = HumanPlayer.new("Frank")
     @computer = ComputerPlayer.new
     @game = Game.new(@human, @computer)
+    $mock_inputs.clear
+    $mock_inputs << "A1"
+    $mock_inputs << "Down"
+    $mock_inputs << "A3"
+    $mock_inputs << "Down"
+    $mock_inputs << "A5"
+    $mock_inputs << "Down"
+    $mock_inputs << "A7"
+    $mock_inputs << "Down"
+    $mock_inputs << "A9"
+    $mock_inputs << "Down"
+    assert_output("Frank, where would you like to place a ship of length 2?\nAcross or Down?\n"+
+                  "Frank, where would you like to place a ship of length 3?\nAcross or Down?\n"+
+                  "Frank, where would you like to place a ship of length 3?\nAcross or Down?\n"+
+                  "Frank, where would you like to place a ship of length 4?\nAcross or Down?\n"+
+                  "Frank, where would you like to place a ship of length 5?\nAcross or Down?\n"+
+                  "HAL 9000 has placed his ships.\n") do
+      @game.place_ships
+    end
+  end
+
+  def test_game_can_have_nonstandard_set_of_ships
+    human = HumanPlayer.new("Alice")
+    computer = ComputerPlayer.new
+    game = Game.new(human, computer, [2, 3])
+    $mock_inputs.clear
+    $mock_inputs << "A1"
+    $mock_inputs << "Down"
+    $mock_inputs << "A3"
+    $mock_inputs << "Down"
+    assert_output("Alice, where would you like to place a ship of length 2?\nAcross or Down?\n"+
+                  "Alice, where would you like to place a ship of length 3?\nAcross or Down?\n"+
+                  "HAL 9000 has placed his ships.\n") do
+      game.place_ships
+    end
+  end
+
+  def test_game_tells_you_if_your_ships_overlap
+    human = HumanPlayer.new("Alice")
+    computer = ComputerPlayer.new
+    game = Game.new(human, computer, [2, 3])
+    $mock_inputs.clear
+    $mock_inputs << "A2"
+    $mock_inputs << "Down"
+    $mock_inputs << "A1"
+    $mock_inputs << "Across"
+    $mock_inputs << "F1"
+    $mock_inputs << "Across"
+    assert_output("Alice, where would you like to place a ship of length 2?\nAcross or Down?\n"+
+                  "Alice, where would you like to place a ship of length 3?\nAcross or Down?\n"+
+                  "Unfortunately, that ship overlaps with one of your other ships.  Please try again.\n"+
+                  "Alice, where would you like to place a ship of length 3?\nAcross or Down?\n"+
+                  "HAL 9000 has placed his ships.\n") do
+      game.place_ships
+    end
+  end
+
+  def test_human_can_take_first_turn
+    set_up_new_game
+    $mock_inputs.clear
+    $mock_inputs << "A1"
+
+    # This /(Miss!|Hit!)/ thing just checks to see if Miss! or Hit! was included anywhere in the message.
+    assert_output(/(Miss!|Hit!)/) do
+      @game.take_turn
+    end
+  end
+
+  def test_computer_can_take_second_turn
+    set_up_new_game
+    $mock_inputs.clear
+    $mock_inputs << "A1"
+    assert_output(/(Miss!|Hit!)/) do
+      @game.take_turn
+    end
+    assert_output(/(Miss!|Hit!)/) do
+      @game.take_turn
+    end
+  end
+
+  def test_display_game_status
+    set_up_new_game
+    assert_output(starting_game_status) do
+      @human.display_game_status
+    end
+  end
+
+  def starting_game_status
+    %Q{    1   2   3   4   5   6   7   8   9   10
+  -----------------------------------------
+A |   |   |   |   |   |   |   |   |   |   |
+B |   |   |   |   |   |   |   |   |   |   |
+C |   |   |   |   |   |   |   |   |   |   |
+D |   |   |   |   |   |   |   |   |   |   |
+E |   |   |   |   |   |   |   |   |   |   |
+F |   |   |   |   |   |   |   |   |   |   |
+G |   |   |   |   |   |   |   |   |   |   |
+H |   |   |   |   |   |   |   |   |   |   |
+I |   |   |   |   |   |   |   |   |   |   |
+J |   |   |   |   |   |   |   |   |   |   |
+  -----------------------------------------
+
+    1   2   3   4   5   6   7   8   9   10
+  -----------------------------------------
+A | O |   | O |   | O |   | O |   | O |   |
+B | O |   | O |   | O |   | O |   | O |   |
+C |   |   | O |   | O |   | O |   | O |   |
+D |   |   |   |   |   |   | O |   | O |   |
+E |   |   |   |   |   |   |   |   | O |   |
+F |   |   |   |   |   |   |   |   |   |   |
+G |   |   |   |   |   |   |   |   |   |   |
+H |   |   |   |   |   |   |   |   |   |   |
+I |   |   |   |   |   |   |   |   |   |   |
+J |   |   |   |   |   |   |   |   |   |   |
+  -----------------------------------------
+}
+  end
+
+  def test_game_status_shows_hits_and_misses
+    human1 = HumanPlayer.new("Amy")
+    human2 = HumanPlayer.new("Beth")
+    game = Game.new(human1, human2, [2])
+    $mock_inputs.clear
+
+    # It doesn't matter what messages come up during the turns
+    assert_output(/./) do
+      $mock_inputs << "A2"     #Amy's ship's location
+      $mock_inputs << "Down"   #Amy's ship's direction
+
+      $mock_inputs << "F3"     #Beth's ship's location
+      $mock_inputs << "Across" #Beth's ship's direction
+      game.place_ships
+
+      $mock_inputs << "F3"     #Amy's hit
+      game.take_turn
+
+      $mock_inputs << "A2"     #Beth's hit
+      game.take_turn
+
+      $mock_inputs << "H4"     #Amy's miss
+      game.take_turn
+
+      $mock_inputs << "A2"     #Beth's miss (she shot in the same spot as last time)
+      game.take_turn
+    end
+
+    # Now the visuals matter.  Should show Amy's shots up top and Amy's own ship below.
+    assert_output(mid_game_status) do
+      human1.display_game_status
+    end
+  end
+
+  def mid_game_status
+    %Q{    1   2   3   4   5   6   7   8   9   10
+  -----------------------------------------
+A |   |   |   |   |   |   |   |   |   |   |
+B |   |   |   |   |   |   |   |   |   |   |
+C |   |   |   |   |   |   |   |   |   |   |
+D |   |   |   |   |   |   |   |   |   |   |
+E |   |   |   |   |   |   |   |   |   |   |
+F |   |   | + |   |   |   |   |   |   |   |
+G |   |   |   |   |   |   |   |   |   |   |
+H |   |   |   | - |   |   |   |   |   |   |
+I |   |   |   |   |   |   |   |   |   |   |
+J |   |   |   |   |   |   |   |   |   |   |
+  -----------------------------------------
+
+    1   2   3   4   5   6   7   8   9   10
+  -----------------------------------------
+A |   | X |   |   |   |   |   |   |   |   |
+B |   | O |   |   |   |   |   |   |   |   |
+C |   |   |   |   |   |   |   |   |   |   |
+D |   |   |   |   |   |   |   |   |   |   |
+E |   |   |   |   |   |   |   |   |   |   |
+F |   |   |   |   |   |   |   |   |   |   |
+G |   |   |   |   |   |   |   |   |   |   |
+H |   |   |   |   |   |   |   |   |   |   |
+I |   |   |   |   |   |   |   |   |   |   |
+J |   |   |   |   |   |   |   |   |   |   |
+  -----------------------------------------
+}
+  end
+
+  def test_game_can_be_won
+    human1 = HumanPlayer.new("Amy")
+    human2 = HumanPlayer.new("Beth")
+    game = Game.new(human1, human2, [2])
+    $mock_inputs.clear
+    $mock_inputs << "A2"     #Amy's ship's location
+    $mock_inputs << "Down"   #Amy's ship's direction
+
+    $mock_inputs << "F3"     #Beth's ship's location
+    $mock_inputs << "Across" #Beth's ship's direction
+
+    $mock_inputs << "F3"     #Amy's first shot
+    $mock_inputs << "A1"     #Beth's first shot
+    $mock_inputs << "F4"     #Amy's winning shot
+
+    assert_output(/Congratulations, Amy!/) do
+      game.play  #When Amy wins, it has to say 'Congratulations, Amy' somewhere in the victory message.
+    end
   end
 end
