552844ec54273f49303d05c9138fd7c222ed001e
Mason F. Matthews <mason.f.matthews@gmail.com>
2015-07-04 08:42:56 -0400

    Set up week 4 challenges.

--- challenges/include_challenge.rb
+++ challenges/include_challenge.rb
@@ -1,10 +1,11 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 
-# Write code which will be included in the human class which will give humans
+# Write modules which will be included in the classes below which will give
 # intelligent and bipedal behavior.
-
-# WRITE YOUR CODE BELOW THIS COMMENT...
+#
+# You may not write any methods in the existing classes, but you may write
+# `include` statements in them.
 
 class Human
   def initialize(name)
@@ -12,14 +13,16 @@ class Human
   end
 end
 
-class ArtificialIntelligence
-  def initialize(name)
+class Ai
+  def initialize(name, version)
     @name = name
+    @version = version
   end
 end
 
 class Ostrich
-  def initialize
+  def initialize(height)
+    @height = height
   end
 end
 
@@ -28,23 +31,33 @@ class IncludeChallenge < MiniTest::Test
   def test_speaking
     assert_equal "My name is Zelda", Human.new("Zelda").say_name
     assert_equal "My name is Mary", Human.new("Mary").say_name
-    assert_equal "My name is Amanda", Human.new("Amanda").say_name
+    assert_equal "My name is Watson", Ai.new("Watson", 1.1).say_name
+    assert_equal "My name is HAL", Ai.new("HAL", 2000).say_name
+    assert_raises(NoMethodError) {Ostrich.new(40).say_name}
   end
 
   def test_running
-    assert_equal "Huff puff huff puff", Human.new("Zelda").run
-    assert_equal "Huff puff huff puff", Human.new("Mary").run
+    assert_equal "Huff puff huff puff", Human.new("Alice").run
+    assert_equal "Huff puff huff puff", Ostrich.new(48.5).run
+    assert_raises(NoMethodError) {Ai.new("Skynet", 3.0).run}
   end
 
+  # So... humor me for a moment and pretend like ostriches don't have brains.
   def test_constants
     assert_equal 2, Human::NUMBER_OF_LEGS
     assert_equal 1, Human::NUMBER_OF_BRAINS
+    assert_equal 2, Ostrich::NUMBER_OF_LEGS
+    assert_equal 1, Ai::NUMBER_OF_BRAINS
   end
 
+  # Again... ostriches get no brains.  My apologies to all ostriches out there.
   def test_methods_like_constants
-    human = Human.new("Zelda")
+    human = Human.new("Cathy")
     assert_equal 2, human.number_of_legs
     assert_equal 1, human.number_of_brains
+    ai = Ai.new("Master Control Program", 1.0)
+    assert_equal 1, ai.number_of_brains
+    assert_raises(NoMethodError) {ai.number_of_legs}
   end
 
 end
